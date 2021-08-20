b9ce7f36a6009226198fcc7da576af66827ba1b2
Ibrahima <58892076+iib0011@users.noreply.github.com>
2021-08-20 03:07:47 +0100

    Update tableEvent.service.ts

--- src/app/services/tableEvent.service.ts
+++ src/app/services/tableEvent.service.ts
@@ -1,7 +1,7 @@
-import { HttpHeaders, HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Injectable, OnInit } from '@angular/core';
-import { LocalDataSource } from 'ng2-smart-table';
-import { SmartTableData } from '../@core/data/smart-table';
+import {HttpHeaders, HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {Injectable, OnInit} from '@angular/core';
+import {LocalDataSource} from 'ng2-smart-table';
+import {SmartTableData} from '../@core/data/smart-table';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +9,7 @@ import { SmartTableData } from '../@core/data/smart-table';
 export class TableEventService {
   settings;
   entity;
-  baseApi   = 'http://localhost:8082/api/';
+  baseApi = 'http://localhost:8082/api/';
 
   source: LocalDataSource = new LocalDataSource();
   headers: HttpHeaders;
@@ -17,18 +17,29 @@ export class TableEventService {
 
   constructor(private service: SmartTableData, private http: HttpClient) {
     const token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIyMiIsImlhdCI6MTYyOTM5Nzk2OSwiZXhwIjoxNjMwMDAyNzY5fQ.VRUhFm6_3gtwEVPEcy_Yi533kP-texrVwBrCCLY5X9ZAFtfneCgUEfE9cDsqGe0LMAVkAQ7tI2H9Q81UrwsV8w';
-    this.headers = new HttpHeaders({ 'Authorization': 'Bearer ' + token }); // create header object
+    this.headers = new HttpHeaders({'Authorization': 'Bearer ' + token}); // create header object
   }
 
   loadEntity(entity, settings) {
     this.entity = entity;
     this.settings = settings;
-    this.http.get<any>(this.baseApi + this.entity + '/', { headers: this.headers }).subscribe(
-      data => {
-        this.source.load(data._embedded[this.entity]);
+    this.http.get<any>(this.baseApi + this.entity + '/', {headers: this.headers}).subscribe(
+      async data => {
+        let initialData: any[] = data._embedded[this.entity];
+        await Promise.all(initialData.map(async item => {
+          let studentPrimeDataInit;
+          const studentPrimeData = await this.http.get<any>(this.baseApi + 'studentPrimes' + '/' + item.createdBy, {headers: this.headers}).toPromise();
+          // console.log(studentPrimeDataInit);
+          item.firstName = studentPrimeData.firstname;
+          //console.log(studentPrimeData);
+          item.lastName = studentPrimeData.lastname;
+        }));
+        console.log(initialData);
+        this.source.load(initialData);
       },
       this.handleError);
   }
+
   onSearch(query: string) {
     this.source.setFilter([
       // fields we want to include in the search
@@ -43,7 +54,7 @@ export class TableEventService {
     if (window.confirm('Are you sure you want to save?')) {
 
       const data = event.newData;
-      this.http.patch<any>(this.baseApi + this.entity + '/' + event.newData.id, data, { headers: this.headers }).subscribe(
+      this.http.patch<any>(this.baseApi + this.entity + '/' + event.newData.id, data, {headers: this.headers}).subscribe(
         async res => {
           console.log(event.data);
           event.confirm.resolve();
@@ -55,11 +66,12 @@ export class TableEventService {
       event.confirm.reject();
     }
   }
+
   onCreateConfirm(event): void {
     if (window.confirm('Are you sure you want to create?')) {
 
       const data = event.newData;
-      this.http.post<any>(this.baseApi + this.entity + '/', data, { headers: this.headers }).subscribe(
+      this.http.post<any>(this.baseApi + this.entity + '/', data, {headers: this.headers}).subscribe(
         async res => {
           console.log(res);
           await this.source.add(res);
@@ -73,17 +85,19 @@ export class TableEventService {
       event.confirm.reject();
     }
   }
+
   handleError(err: HttpErrorResponse) {
     if (err.error instanceof Error) {
-      console.log('Client-side error occured.',err);
+      console.log('Client-side error occured.', err);
     } else {
-      console.log('Server-side error occured.',err);
+      console.log('Server-side error occured.', err);
     }
 
   }
+
   onDeleteConfirm(event): void {
     if (window.confirm('Are you sure you want to delete?')) {
-      this.http.delete<any>(this.baseApi + this.entity + '/' + event.data.id, { headers: this.headers }).subscribe(
+      this.http.delete<any>(this.baseApi + this.entity + '/' + event.data.id, {headers: this.headers}).subscribe(
         async data => {
           console.log(data);
           await this.source.remove(event.data);
