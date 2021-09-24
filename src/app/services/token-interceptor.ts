f3c75ade69fc7957bf9c5147d7926e1b17454cf5
Ibrahima <ibracool99@gmail.com>
2021-09-24 07:49:07 +0100

    token in interceptor

--- src/app/services/token-interceptor.ts
+++ src/app/services/token-interceptor.ts
@@ -1,15 +1,27 @@
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
 import {Injectable} from '@angular/core';
+import {NbAuthJWTToken, NbAuthService} from '@nebular/auth';
+import {ApiService} from './api.service';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
+  token: NbAuthJWTToken;
+
+  constructor(private authService: NbAuthService) {
+    this.authService.onTokenChange()
+      .subscribe((token: NbAuthJWTToken) => {
+
+        if (token.isValid()) {
+          this.token = token;
+        }
+      });
+  }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI3IiwiaWF0IjoxNjMyNDI0Nzk1LCJleHAiOjE2MzMwMjk1OTV9._4Qbx_78JMdrR12rPNv0HHhhQ_6Hq_6b3tAxn-KrLe95kXcAQEDCCXw4FiZmdtiYcesOHCtvEdG6FUvbE28aGg';
     const modifiedReq = req.clone({
-      headers: req.headers.set('Authorization', `Bearer ${token}`),
+      headers: req.headers.set('Authorization', `Bearer ${this.token}`),
     });
     return next.handle(modifiedReq);
   }
