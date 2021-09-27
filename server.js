3b5035f1160f6ad7b14b72799317f201a9e9b6d8
ambou-37 <ambroise.tine@gmail.com>
2021-09-27 21:45:56 +0100

    dist

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(requireHTTPS);
 
 //2. Serve our static files.
-app.use(express.static('./dist/ngx-admin'));
+app.use(express.static('./dist/'));
 //3. Wait for a request to any path and redirect all of the requests to index.html
 app.get('/*', function(req, res) {
   res.sendFile('index.html', {root: 'dist/'}
