4a629540a6fd5557ad82f5eadc278262b0fc1429
Ibrahima <ibracool99@gmail.com>
2021-09-28 00:42:05 +0100

    custom login

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { NgxAuthRoutingModule } from './auth-routing.module';
+import { NgxLoginComponent } from './login/login.component'; // <---
+import { NbAuthModule } from '@nebular/auth';
+import {
+  NbAlertModule,
+  NbButtonModule,
+  NbCheckboxModule,
+  NbInputModule
+} from '@nebular/theme';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    NbAlertModule,
+    NbInputModule,
+    NbButtonModule,
+    NbCheckboxModule,
+    NgxAuthRoutingModule,
+
+    NbAuthModule,
+  ],
+  declarations: [
+
+    NgxLoginComponent
+  ],
+})
+export class NgxAuthModule {
+}
