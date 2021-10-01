cd60ea4d4b4361e3ec4c20a92cff992d90a2b2d7
Ibrahima <ibracool99@gmail.com>
2021-10-01 08:30:27 +0100

    ProfileModule

---
+++ src/app/pages/profile/profile.module.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { NbAuthModule } from '@nebular/auth';
+import {
+  NbAlertModule,
+  NbButtonModule,
+  NbCheckboxModule,
+  NbInputModule
+} from '@nebular/theme';
+import {ProfileComponent} from './profile.component';
+
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    NbAlertModule,
+    NbInputModule,
+    NbButtonModule,
+    NbCheckboxModule,
+    NbAuthModule,
+  ],
+  declarations: [
+
+  ProfileComponent
+  ],
+})
+export class ProfileModule {
+}
